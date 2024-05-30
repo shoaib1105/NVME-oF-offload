ce015f7491cd9be8db67b767e9c26842b08ac878
shoaib1105 <49727028+shoaib1105@users.noreply.github.com>
2024-05-30 11:57:55 -0400

    Update install_ofed.sh

--- install_ofed.sh
+++ install_ofed.sh
@@ -1,39 +1,42 @@
-#!/bin/bash
+wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
+tar -xvzf MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
 
-# ofed-ubuntu.sh
-# Install Mellanox Ofed on Ubuntu Machines
-#
-# Author: Nilson Lopes (06/17/2021)
+# #!/bin/bash
 
-ubuntu_release=$(awk -F '"' '/VERSION_ID/ {print $2}' /etc/os-release)
-mofed_package_version='latest'
-mofed_repo_base_url="https://linux.mellanox.com/public/repo/mlnx_ofed"
-mofed_repo_gpg_url="https://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox"
-ubuntu_repo_dist_name="ubuntu${ubuntu_release}"
-ubuntu_repo_file_name="mellanox_mlnx_ofed.list"
-ubuntu_repo_file_path="/etc/apt/sources.list.d/${ubuntu_repo_file_name}"
+# # ofed-ubuntu.sh
+# # Install Mellanox Ofed on Ubuntu Machines
+# #
+# # Author: Nilson Lopes (06/17/2021)
 
-echo "adding apt key"
-wget -qO - ${mofed_repo_gpg_url} | apt-key add -
+# ubuntu_release=$(awk -F '"' '/VERSION_ID/ {print $2}' /etc/os-release)
+# mofed_package_version='latest'
+# mofed_repo_base_url="https://linux.mellanox.com/public/repo/mlnx_ofed"
+# mofed_repo_gpg_url="https://www.mellanox.com/downloads/ofed/RPM-GPG-KEY-Mellanox"
+# ubuntu_repo_dist_name="ubuntu${ubuntu_release}"
+# ubuntu_repo_file_name="mellanox_mlnx_ofed.list"
+# ubuntu_repo_file_path="/etc/apt/sources.list.d/${ubuntu_repo_file_name}"
 
-url="${mofed_repo_base_url}/${mofed_package_version}/${ubuntu_repo_dist_name}/${ubuntu_repo_file_name}"
-wget -qO ${ubuntu_repo_file_path} ${url}
-chmod 644 ${ubuntu_repo_file_path}
+# echo "adding apt key"
+# wget -qO - ${mofed_repo_gpg_url} | apt-key add -
 
-echo "update apt cache"
-apt update -y
+# url="${mofed_repo_base_url}/${mofed_package_version}/${ubuntu_repo_dist_name}/${ubuntu_repo_file_name}"
+# wget -qO ${ubuntu_repo_file_path} ${url}
+# chmod 644 ${ubuntu_repo_file_path}
 
-echo "install packages"
-OFED_PKGS=(libc6-dev
- mlnx-ofed-kernel-dkms
- mlnx-ofed-kernel-utils
- mlnx-ofed-basic
- rdma-core
- ibverbs-utils
- ibverbs-providers
-)
+# echo "update apt cache"
+# apt update -y
 
-apt install -y ${OFED_PKGS[@]}
+# echo "install packages"
+# OFED_PKGS=(libc6-dev
+#  mlnx-ofed-kernel-dkms
+#  mlnx-ofed-kernel-utils
+#  mlnx-ofed-basic
+#  rdma-core
+#  ibverbs-utils
+#  ibverbs-providers
+# )
 
-echo "configure ib devices ..."
-systemctl restart openibd
+# apt install -y ${OFED_PKGS[@]}
+
+# echo "configure ib devices ..."
+# systemctl restart openibd
