0ce8e3653ce4c89f65735c1dbb3ec1342c1aaf1a
shoaib1105 <49727028+shoaib1105@users.noreply.github.com>
2024-05-30 12:33:22 -0400

    Update install_ofed.sh

--- install_ofed.sh
+++ install_ofed.sh
@@ -1,6 +1,7 @@
 wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
 tar -xvzf MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
 sudo ./mlnxofedinstall --add-kernel-support --with-nvmf
+/etc/init.d/openibd restart
 
 # #!/bin/bash
 
