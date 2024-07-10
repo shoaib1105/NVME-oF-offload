fa56795768df5d0545c6418543140a25522dd152
shoaib1105 <49727028+shoaib1105@users.noreply.github.com>
2024-07-10 19:47:23 -0400

    Update install_ofed.sh

--- install_ofed.sh
+++ install_ofed.sh
@@ -1,6 +1,6 @@
 #Download MLNX OFED
-wget https://content.mellanox.com/ofed/MLNX_OFED-20.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu22.04-x86_64.tgz
-tar -xvzf MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-20.10-0.5.5.0-ubuntu22.04-x86_64.tgz
+wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
+tar -xvzf MLNX_OFED-23.10-0.5.5.0/MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu20.04-x86_64.tgz
 cd MLNX_OFED_LINUX-23.10-0.5.5.0-ubuntu22.04-x86_64/
 sudo apt update
 sudo apt install bzip2
