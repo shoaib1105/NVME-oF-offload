21f48d34ebe258ffc843f4ae45a40dec84972ebb
shoaib1105 <49727028+shoaib1105@users.noreply.github.com>
2024-07-11 15:34:40 -0400

    Create client_config.sh

---
+++ client_config.sh
@@ -0,0 +1,15 @@
+modprobe nvme
+
+modprobe nvme-rdma
+
+ 
+
+nvme discover -t rdma -a 10.0.1.2 -s 4420
+
+nvme connect -t rdma -n testsubsystem -a 10.0.1.2 -s 4420
+
+ 
+
+nvme discover -t rdma -a 10.0.2.2 -s 4420
+
+nvme connect -t rdma -n testsubsystem2 -a 10.0.2.2 -s 4420
